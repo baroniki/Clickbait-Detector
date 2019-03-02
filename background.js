92cde43a7aba398272798776072b60de72e030b3
Alex Baronikian <baroniki@usc.edu>
2019-03-02 03:01:15 -0800

    Detection for hovering over videos

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
-chrome.runtime.onInstalled.addListener(function() {
-    alert("i want to die");
+alert("i want to die");
+
+const videos = document.querySelectorAll("div#dismissable.style-scope.ytd-grid-video-renderer");
+console.log(videos);
+videos.forEach((el) => {
+    el.addEventListener("mouseover", (event) => { showClickbaitInfo(event.target) });
 });
+
+function showClickbaitInfo(element) {
+    alert("i seek death");
+}
